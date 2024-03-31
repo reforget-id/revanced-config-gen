2b3071b1d3d2b1e45dead9cfdc3b8e213b9dcf14
reforget-id <58993868+reforget-id@users.noreply.github.com>
2024-03-31 20:27:15 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	},
   preprocess: vitePreprocess(),
-}
+};
+
+export default config;
